098730721ebf70ba4e8f40c7681df5dc62fe39b8
agiskorska <skorskaga@gmail.com>
2022-07-18 12:15:02 +0100

    Add post route to server

--- server/app.js
+++ server/app.js
@@ -10,6 +10,19 @@ app.use(cors());
 
 app.get(["/", "/home"], (req,res) => {
     res.json(data)
-})
+});
+
+
+app.post( "/newuser", (req, res) => {
+    const newEntry = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        gender: req.body.gender,
+        ip_address: req.body.ip_address
+    };
+    data.push(newEntry);
+    res.status(201).send(newEntry);
+});
 
 module.exports = app;
